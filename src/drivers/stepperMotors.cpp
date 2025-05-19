4bd248596091e26c625439e1ce50097ec78db259
Andrew Grasman <andrew.grasman@gmial.com>
2025-05-19 11:53:15 -0400

    Stepper motor init

--- src/drivers/stepperMotors.cpp
+++ src/drivers/stepperMotors.cpp
@@ -3,3 +3,11 @@
  */
 
 #include "stepperMotors.h"
+#define stepsPerRevolution 32*32 //full step drive
+
+Stepper hourStep(stepsPerRevolution,39,40,41,42);
+Stepper tensStep(stepsPerRevolution,35,36,37,38);
+Stepper minsStep(stepsPerRevolution,20,21,47,48);
+void initStepperMotors(){
+
+}
